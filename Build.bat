5b18f26bd76d2cbc06f9551213de3690ae2dff13
HoratioVex <38878728+HoratioVex@users.noreply.github.com>
2024-05-02 20:37:59 +0200

    Add files via upload

---
+++ Build.bat
@@ -0,0 +1,9 @@
+cd ASM
+.\asar.exe .\Earthlight-Main.asm "..\Earth Light (English).sfc"
+cd..
+cd Script
+atlas "..\Earth Light (English).sfc" "Earthlight1-MainText.txt"
+atlas "..\Earth Light (English).sfc" "Earthlight1-LabelsText.txt"
+atlas "..\Earth Light (English).sfc" "Earthlight1-Briefing.txt"
+atlas "..\Earth Light (English).sfc" "Earthlight1-UnitNames.txt"
+cd..
